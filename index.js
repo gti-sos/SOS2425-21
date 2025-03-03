17d0e6d7f9dad2ba726e1875795a2a60a4a48e18
Laura Eraso <laueralor@alum.us.es>
2025-03-03 12:08:38 +0100

    L04

--- index.js
+++ index.js
@@ -6,12 +6,12 @@ const app = express();
 const PORT = process.env.PORT || 16078;
 
 
-app.use("/about", express.static(__dirname + "/public"));
+app.use("/about", express.static(__dirname + "/public/about.html"));
 
 app.get('/', (request, response) =>{
     response.send(`Este es el servidor del <a href="/about">grupo 21</a><br>
         <a href="/cool">Cool</a><br>
-        <a href="/samples/EBT">Algoritmo EBT</a>`);
+        <a href="/samples/LEL">Algoritmo LEL</a>`);
 }
 );
 
