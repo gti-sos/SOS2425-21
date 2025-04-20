03d4ada512075e80fc4e3a2ca35948399b3aeb29
Laura Eraso <laueralor@alum.us.es>
2025-04-20 20:28:41 +0200

    .

--- src/back/home-buying-selling-stats.js
+++ src/back/home-buying-selling-stats.js
@@ -1,57 +1,58 @@
 const BASE_API = "/api/v1";
 import DataStore from "nedb";
-import fs from "fs";
-import csv from "csv-parser";
+
 
 
 let db_LEL = new DataStore({ filename: "homeBuyingSellingStats.db", autoload: true });
 const RESOURCE = "home-buying-selling-stats";
 
-//Función para leer datos del CSV
-const readCSVDataLEL = () => {
-    return new Promise((resolve, reject) => {
-        const results = [];
-        fs.createReadStream("data/SOS2425-21-Propuesta - Laura.csv") 
-            .pipe(csv())
-            .on("data", (row) => {
-                try {
-                    const formattedRow = {
-                        year: parseInt(row.year),
-                        province: row.province,
-                        transaction_total: parseInt(row.transaction_total.replace(/\D/g, "")),
-                        transaction_protected_housing: parseInt(row.transaction_protected_housing.replace(/\D/g, "")),
-                        transaction_new_housing: parseInt(row.transaction_new_housing.replace(/\D/g, "")),
-                        transaction_secondhand_housing: parseInt(row.transaction_secondhand_housing.replace(/\D/g, ""))
-                    };
-                    results.push(formattedRow);
-                } catch (error) {
-                    console.error("Error procesando fila:", row, error);
-                }
-            })
-            .on("end", () => resolve(results))
-            .on("error", (err) => reject(err));
-    });
-};
-
-function loadBackendLEL(app){
-    // Endpoint para cargar los datos iniciales desde el CSV
-    app.get(`${BASE_API}/${RESOURCE}/loadInitialData`, async (req, res) => {
-        db_LEL.count({}, async (err, count) => {
-            if (err) return res.status(500).json({ error: "Error al contar registros." });
-            if (count > 0) return res.status(409).json({ error: "Los datos ya están cargados." });
-
-            try {
-                const data = await readCSVDataLEL();
-                db_LEL.insert(data, (err) => {
-                    if (err) return res.status(500).json({ error: "Error al insertar los datos." });
-                    res.status(201).json({ message: "Datos iniciales cargados correctamente." });
-                });
-            } catch (error) {
-                console.error("Error al leer CSV:", error);
-                res.status(500).json({ error: "Error interno al cargar los datos." });
+const initialDataLEL = [
+    { year: 2024, province: "alicante", transaction_total: 42176, transaction_protected_housing: 998, transaction_new_housing: 4687, transaction_secondhand_housing: 37489 },
+    { year: 2024, province: "las palmas", transaction_total: 10702, transaction_protected_housing: 216, transaction_new_housing: 612, transaction_secondhand_housing: 10090 },
+    { year: 2024, province: "madrid", transaction_total: 63218, transaction_protected_housing: 1203, transaction_new_housing: 5720, transaction_secondhand_housing: 57498 },
+    { year: 2024, province: "malaga", transaction_total: 27791, transaction_protected_housing: 377, transaction_new_housing: 3482, transaction_secondhand_housing: 24309 },
+    { year: 2024, province: "pontevedra", transaction_total: 5874, transaction_protected_housing: 49, transaction_new_housing: 607, transaction_secondhand_housing: 5267 },
+    { year: 2024, province: "barcelona", transaction_total: 49950, transaction_protected_housing: 913, transaction_new_housing: 4268, transaction_secondhand_housing: 45682 },
+    { year: 2024, province: "zaragoza", transaction_total: 9529, transaction_protected_housing: 165, transaction_new_housing: 821, transaction_secondhand_housing: 8708 },
+    { year: 2024, province: "badajoz", transaction_total: 5471, transaction_protected_housing: 440, transaction_new_housing: 168, transaction_secondhand_housing: 5303 },
+    { year: 2019, province: "alicante", transaction_total: 42418, transaction_protected_housing: 1152, transaction_new_housing: 5456, transaction_secondhand_housing: 36962 },
+    { year: 2019, province: "las palmas", transaction_total: 12883, transaction_protected_housing: 428, transaction_new_housing: 1004, transaction_secondhand_housing: 11879 },
+    { year: 2019, province: "madrid", transaction_total: 78634, transaction_protected_housing: 3628, transaction_new_housing: 10598, transaction_secondhand_housing: 68036 },
+    { year: 2019, province: "malaga", transaction_total: 30876, transaction_protected_housing: 908, transaction_new_housing: 3657, transaction_secondhand_housing: 27219 },
+    { year: 2019, province: "pontevedra", transaction_total: 6554, transaction_protected_housing: 113, transaction_new_housing: 529, transaction_secondhand_housing: 6025 },
+    { year: 2019, province: "barcelona", transaction_total: 59554, transaction_protected_housing: 1186, transaction_new_housing: 5017, transaction_secondhand_housing: 54537 },
+    { year: 2019, province: "zaragoza", transaction_total: 10787, transaction_protected_housing: 280, transaction_new_housing: 1278, transaction_secondhand_housing: 9509 },
+    { year: 2019, province: "badajoz", transaction_total: 5795, transaction_protected_housing: 567, transaction_new_housing: 263, transaction_secondhand_housing: 5532 },
+    { year: 2015, province: "alicante", transaction_total: 31677, transaction_protected_housing: 594, transaction_new_housing: 3769, transaction_secondhand_housing: 27908 },
+    { year: 2015, province: "las palmas", transaction_total: 10745, transaction_protected_housing: 341, transaction_new_housing: 1097, transaction_secondhand_housing: 9648 },
+    { year: 2015, province: "madrid", transaction_total: 57431, transaction_protected_housing: 3255, transaction_new_housing: 6820, transaction_secondhand_housing: 50611 },
+    { year: 2015, province: "malaga", transaction_total: 25798, transaction_protected_housing: 600, transaction_new_housing: 2636, transaction_secondhand_housing: 23162 },
+    { year: 2015, province: "pontevedra", transaction_total: 4421, transaction_protected_housing: 128, transaction_new_housing: 700, transaction_secondhand_housing: 3721 },
+    { year: 2015, province: "barcelona", transaction_total: 41342, transaction_protected_housing: 888, transaction_new_housing: 3472, transaction_secondhand_housing: 37870 },
+    { year: 2015, province: "zaragoza", transaction_total: 7952, transaction_protected_housing: 191, transaction_new_housing: 848, transaction_secondhand_housing: 7104 },
+    { year: 2015, province: "badajoz", transaction_total: 4044, transaction_protected_housing: 622, transaction_new_housing: 587, transaction_secondhand_housing: 3457 }
+];
+db_LEL.find({}, (err, records) => {
+    if (err) {
+        console.error("Error al obtener los registros: ", err);
+        return;
+    }
+    if (records.length < 1) {
+        console.log("Insertando datos iniciales...");
+        db_LEL.insert(initialDataLEL, (err) => {
+            if (err) {
+                console.error("Error al insertar los datos iniciales:", err);
+            } else {
+                console.log("Datos iniciales insertados correctamente.");
             }
         });
-    });
+    } else {
+        console.log("Datos ya presentes en la base de datos.");
+    }
+});
+
+function loadBackendLEL(app){
+    
     // GET - Obtener todos los datos con paginación y filtrado por todos los campos
     app.get(`${BASE_API}/${RESOURCE}`, (req, res) => {
         const limit = parseInt(req.query.limit) || 0;   // 0 significa sin límite
@@ -76,8 +77,12 @@ function loadBackendLEL(app){
                     if (err) return res.status(500).json({ error: "Error al obtener los datos." });
 
                     const cleanDocs = docs.map(({ _id, ...rest }) => rest);
-                    res.status(200).json(cleanDocs);
-
+                    res.status(200).json({
+                        total: totalCount,
+                        limit,
+                        offset,
+                        data: cleanDocs
+                    });
                 });
         });
     });
@@ -92,11 +97,9 @@ function loadBackendLEL(app){
         const province = req.params.province;
         db_LEL.find({ province: new RegExp(`^${province}$`, "i") }, (err, docs) => {
             if (err) return res.status(500).json({ error: "Error al buscar la provincia." });
-            if (!docs || docs.length === 0) return res.status(404).json({ error: "Provincia no encontrada." });
-            
-            // Quitamos el _id de cada documento
-            const cleanDocs = docs.map(({ _id, ...rest }) => rest);
-            res.status(200).json(cleanDocs);
+            if (!doc) return res.status(404).json({ error: "Provincia no encontrada." });
+            const { _id, ...cleanDoc } = doc;
+            res.status(200).json(cleanDoc);
         });
     });
 
@@ -105,7 +108,7 @@ function loadBackendLEL(app){
         const province = req.params.province.toLowerCase();
         const year = parseInt(req.params.year);
     
-        db_LEL.findOne({ province: new RegExp(`^${province}$`, "i"), year }, (err, doc) => {
+        db_LEL.find({ province: new RegExp(`^${province}$`, "i"), year }, (err, doc) => {
             if (err) return res.status(500).json({ error: "Error al buscar la estadística." });
             if (!doc) return res.status(404).json({ error: "Estadística no encontrada." });
     
