e9ee4fe4ce3db747bcc95722fe0fdb44a4bc8d25
andgombal <andgombal@alum.us.es>
2025-04-25 12:37:53 +0200

    cambio about

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
 import express from "express";
 import cors from "cors";
-import { fileURLToPath } from "url";
-import { dirname } from "path";
 import { loadBackendAGB } from "./src/back/public-transit.js";
 import { loadBackendPRG } from "./src/back/cultural-event.js";
 import { loadBackendLEL } from "./src/back/home-buying-selling-stats.js";
 import { handler } from './src/front/build/handler.js';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
 
 const app = express();
 const PORT = process.env.PORT || 16078;
@@ -18,19 +14,16 @@ const BASE_API = "/api/v1";
 app.use(express.json());
 app.use(cors());
 //app.use("/", express.static(__dirname));
-
-app.get("/", (req, res) => {
-    res.redirect("/about");
-  });
   
 
 loadBackendAGB(app);
 loadBackendPRG(app); // 👈 NUEVA LÍNEA
 loadBackendLEL(app);
 
-app.use(handler);
+
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
 
+app.use(handler);
