fe578ba9923f80f189ab5d48fe65b28db2c4734e
pauruigar6 <pauruigar6@alum.us.es>
2025-05-10 22:17:56 +0200

    Added cultural-event at Header.svelte

--- src/front/src/routes/Header.svelte
+++ src/front/src/routes/Header.svelte
@@ -2,3 +2,4 @@
 <a href="/">Home</a>
 <a href="/public-transit-stats">Estadísticas sobre los viajes en autobús urbano en España</a>
 <a href="/home-buying-selling-stats">Estadísticas sobre la compraventa de viviendas en España</a>
+<a href="/cultural-event">Estadísticas sobre los eventos culturales en España</a>
